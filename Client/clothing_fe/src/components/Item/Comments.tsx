dcd65b8e78b5616ebee328b38a54feb02dc73f5c
Đồng Thanh Tuyền <dongtuyen6202@gmail.com>
2026-09-21 21:35:08 +0700

    update fe item page's comment section and api handler

--- Client/clothing_fe/src/components/Item/Comments.tsx
+++ Client/clothing_fe/src/components/Item/Comments.tsx
@@ -11,21 +11,43 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Pagination from '@mui/material/Pagination';
 import Rating from '@mui/material/Rating';
-
+import Box from '@mui/material/Box';
+import StarIcon from '@mui/icons-material/Star';
+import Stack from '@mui/material/Stack';
 
 //
-import ModeCommentIcon from '@mui/icons-material/ModeComment';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 //
 import './Css/Comment.css'
+import { Button } from '@mui/material';
 //
 const options = [
   'Report',
 ];
 
+const labels: { [index: string]: string } = {
+    0.5: 'Useless',
+    1: 'Useless+',
+    1.5: 'Poor',
+    2: 'Poor+',
+    2.5: 'Ok',
+    3: 'Ok+',
+    3.5: 'Good',
+    4: 'Good+',
+    4.5: 'Excellent',
+    5: 'Excellent+',
+};
+
+function getLabelText(value: number) {
+    return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+}
 
 export default function Comment(){
+    //
+    const [value, setValue] = React.useState<number | null>(4);
+    const [hover, setHover] = React.useState(-1);
+
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
@@ -36,17 +58,79 @@ export default function Comment(){
     };
     return(
         <>
-        <div className="w-full p-4 flex flex-col gap-4">
-            <div className="flex gap-2 items-center">
+        <div className="w-full p-4 flex flex-col items-center gap-4">
+            {/* <div className="flex gap-2 items-center">
                 <ModeCommentIcon/>
                 <h1 className="text-xl 
                 font-bold 
                 text-black leading-tight tracking-tight">
                    Đánh Giá - Nhận Xét Từ Khách Hàng 
                 </h1>
+            </div> */}
+            <div className="w-full lg:w-3/4 flex flex-col gap-4 p-4 rounded-md comment-box">
+                <div className="flex justify-center">
+                    <h1 className="text-xl 
+                    font-bold 
+                    text-black leading-tight tracking-tight">
+                        Đánh Giá - Nhận Xét Từ Khách Hàng 
+                    </h1>
+                </div>
+                <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+                    <div className="flex flex-col gap-2 items-center">
+                        <Box sx={{ width: 200, display: 'flex', alignItems: 'center' }}>
+                            <Rating
+                            name="hover-feedback"
+                            value={value}
+                            precision={0.5}
+                            getLabelText={getLabelText}
+                            onChange={(event, newValue) => {
+                            setValue(newValue);
+                            }}
+                            onChangeActive={(event, newHover) => {
+                            setHover(newHover);
+                            }}
+                            emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
+                            />
+                        {value !== null && (
+                        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+                        )}
+                        </Box>
+                        <p>Based on 27 reviews</p>
+                    </div>
+                    <div className="items-center flex flex-col text-[13px]">
+                        <Stack spacing={1}>
+                            <div className="flex gap-4">
+                                <Rating name="size-small" defaultValue={5} readOnly size="small" />
+                                <p>from 24 customer</p>
+                            </div>
+                            <div className="flex gap-4">
+                                <Rating name="size-small" defaultValue={4} readOnly size="small" />
+                                <p>from 3 customer</p>
+                            </div>
+                            <div className="flex gap-4">
+                                <Rating name="size-small" defaultValue={3} readOnly size="small" />
+                                <p>from 0 customer</p>
+                            </div>
+                            <div className="flex gap-4">
+                                <Rating name="size-small" defaultValue={2} readOnly size="small" />
+                                <p>from 0 customer</p>
+                            </div>
+                            <div className="flex gap-4">
+                                <Rating name="size-small" defaultValue={1} readOnly size="small" />
+                                <p>from 0 customer</p>
+                            </div>
+                        </Stack>
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <Button sx={{backgroundColor: '#00B7CD', 
+                        color: 'white'}}
+                        >Write A review</Button>
+                    </div>
+                </div>
+
             </div>
             
-            <div className="w-1/3 p-4 rounded-lg comment-box">
+            <div className="w-3/4 p-4 rounded-lg comment-box">
                 <select className='text-[#00B7CD]'>
                     <option value="">This</option>
                     <option value="">This</option>
@@ -116,6 +200,7 @@ export default function Comment(){
                 <Pagination sx={{backgroundColor: 'transparent'}}  
                 count={10} showFirstButton showLastButton />
             </div>
+            
         </div>
         </>
     )
