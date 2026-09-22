9e707f26949c4354a93655fcf4be8a809bb7492e
Đồng Thanh Tuyền <dongtuyen6202@gmail.com>
2026-09-22 15:17:48 +0700

    update for discount

--- Server/clothing_be/clothing_be/Models/productions/DiscountModel.cs
+++ Server/clothing_be/clothing_be/Models/productions/DiscountModel.cs
@@ -1,4 +1,5 @@
 ﻿using clothing_be.Models.Others;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
@@ -17,10 +18,11 @@ namespace clothing_be.Models.productions
 
         [Required]
         [MaxLength(3000, ErrorMessage = "Nội dung bình luận không được vượt quá 3000 ký tự")]
-        public string Descrtiption { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
 
-        [Required, MaxLength(255)]
+        [MaxLength(255)]
         public string? Codes { get; set; }
+
         [Required]
         [Range(0, 100)]
         [Column(TypeName = "decimal(5, 2)")]
@@ -31,10 +33,18 @@ namespace clothing_be.Models.productions
 
         [Required]
         public DateTime StartAt {  get; set; }
+       
         [Required]
-        public DateTime ExpireAt {  get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public DateTime ExpireAt => StartAt.Add(Duration);
 
         //
+        public int? ImageId { get; set; }
+        [ForeignKey(nameof(ImageId))]
+        [JsonIgnore]
+        public virtual ImageModel? Image { get; set; }
+
         [Required]
         public int StatusId { get; set; }
         [ForeignKey(nameof(StatusId))]
